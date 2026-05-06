d331b8c29d74e3ee93f262b93a1f8e6f5ec27dfc
Sharwien <sharwien@legalgen.com>
2026-05-06 18:23:38 -0300

    test3

--- custom/addons/invoice_currency_recalculate/__manifest__.py
+++ custom/addons/invoice_currency_recalculate/__manifest__.py
@@ -2,6 +2,9 @@
     "name": "Invoice Currency Recalculate",
     "version": "18.0.1.0.0",
     "depends": ["account"],
+    "data": [
+        "views/account_move_view.xml",
+    ],
     "installable": True,
     "application": False,
 }
--- custom/addons/invoice_currency_recalculate/models/account_move.py
+++ custom/addons/invoice_currency_recalculate/models/account_move.py
@@ -4,22 +4,29 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    previous_currency_id = fields.Many2one(
+        "res.currency",
+        string="Previous Currency"
+    )
+
+    @api.onchange("invoice_line_ids")
+    def _onchange_invoice_line_ids_set_previous_currency(self):
+        for move in self:
+            if move.currency_id and not move.previous_currency_id:
+                move.previous_currency_id = move.currency_id
+
     @api.onchange("currency_id")
     def _onchange_currency_id_recalculate_invoice_lines(self):
         for move in self:
-            if not move.invoice_line_ids:
-                continue
-
             new_currency = move.currency_id
-
-            # Try to get old currency.
-            # If this is a new unsaved invoice, _origin may be empty.
-            old_currency = move._origin.currency_id or move.company_id.currency_id
+            old_currency = move.previous_currency_id or move._origin.currency_id
 
             if not old_currency or not new_currency:
+                move.previous_currency_id = new_currency
                 continue
 
             if old_currency == new_currency:
+                move.previous_currency_id = new_currency
                 continue
 
             date = move.invoice_date or fields.Date.context_today(move)
@@ -38,3 +45,5 @@ class AccountMove(models.Model):
                 )
 
                 line.price_unit = new_currency.round(new_price)
+
+            move.previous_currency_id = new_currency
