0a9773c57fd1a9cd9110387fa970424c707fbf00
Sharwien <sharwien@legalgen.com>
2026-05-06 22:33:30 -0300

    test4

--- custom/addons/invoice_product_currency_report/__manifest__.py
+++ custom/addons/invoice_product_currency_report/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "18.0.1.0.0",
     "depends": ["account"],
     "data": [
+        "security/ir.model.access.csv",
         "views/invoice_product_currency_report_views.xml",
     ],
     "installable": True,
