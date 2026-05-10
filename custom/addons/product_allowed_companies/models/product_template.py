f4c552ad7963c93627bf30d00f43b231851b2db8
Sharwien <sharwien@legalgen.com>
2026-05-09 23:11:53 -0300

    test5

--- custom/addons/product_allowed_companies/models/product_template.py
+++ custom/addons/product_allowed_companies/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -13,6 +13,33 @@ class ProductTemplate(models.Model):
         help="If empty, the product is visible to all companies. "
              "If set, the product is visible only to the selected companies.",
     )
+
+    display_currency_id = fields.Many2one(
+        "res.currency",
+        string="Display Currency",
+        compute="_compute_display_currency_id",
+        store=False,
+    )
+
+    @api.depends("company_id", "allowed_company_ids")
+    def _compute_display_currency_id(self):
+        usd = self.env.ref("base.USD", raise_if_not_found=False)
+
+        for product in self:
+            if product.company_id:
+                product.display_currency_id = product.company_id.currency_id
+            elif product.allowed_company_ids:
+                currencies = product.allowed_company_ids.mapped("currency_id")
+                if len(currencies) == 1:
+                    product.display_currency_id = currencies[0]
+                elif usd:
+                    product.display_currency_id = usd
+                else:
+                    product.display_currency_id = self.env.company.currency_id
+            elif usd:
+                product.display_currency_id = usd
+            else:
+                product.display_currency_id = self.env.company.currency_id
 
 
 class ProductProduct(models.Model):
