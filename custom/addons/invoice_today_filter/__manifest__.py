457d2108fee578854361fe09100b93e44cd118d1
Sharwien <sharwien@legalgen.com>
2026-05-11 22:38:07 -0300

    test1

---
+++ custom/addons/invoice_today_filter/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Invoice Analysis Today Filter",
+    "version": "1.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [
+        "views/invoice_report_search_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
